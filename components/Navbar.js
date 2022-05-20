a45bd8d308317a39a4d8dcd2679bfdfda9f75d63
KIMKICHAN <rlcks980705@gmail.com>
2022-05-20 15:22:35 +0900

    feat : component

---
+++ components/Navbar.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+//ae0aa2e47e20a0f5d98f268b4c043041
+function Navbar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <img src="/favicon.ico" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+    </nav>
+  );
+}
+
+export default Navbar;
